a5f2233f9e1480c11d44216abf23664ee2bd6fe3
Dmitry Ilyin <dima@doty.ru>
2020-02-06 14:45:12 +0300

    Add option for WASM SIMD support

--- debug_d8.sh
+++ debug_d8.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 pushd build
-~/git/v8/v8/out.gn/x64.release/d8 --experimental-wasm-threads debug_h265.js
+~/git/v8/v8/out.gn/x64.release/d8 \
+  --experimental-wasm-threads \
+  --experimental-wasm-simd \
+  debug_h265.js
 popd
